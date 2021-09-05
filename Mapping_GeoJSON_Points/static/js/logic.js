360179ec539c31aab94573505c42d34955d31629
Austin-Cyr <84824391+Austin-Cyr@users.noreply.github.com>
2021-09-05 12:58:01 -0500

    updating map with street/dark

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -2,23 +2,63 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+// let map = L.map('mapid').setView([30, 30], 2);
 
-// Coordinates for each point to be used in the line.
-let line = [
-    [33.9416, -118.4085],
-    [37.6213, -122.3790],
-    [40.7899, -111.9791],
-    [47.4502, -122.3088]
-  ];
 
-// Create a polyline using the line coordinates and make the line red.
-L. polyline(line, {
-    color: "blue",
-    opacity: .5,
-    weight: 4,
-    dashArray: 1,
-}).addTo(map);
+
+// // Add GeoJSON data.
+// let sanFranAirport =
+// {"type":"FeatureCollection","features":[{
+//     "type":"Feature",
+//     "properties":{
+//         "id":"3469",
+//         "name":"San Francisco International Airport",
+//         "city":"San Francisco",
+//         "country":"United States",
+//         "faa":"SFO",
+//         "icao":"KSFO",
+//         "alt":"13",
+//         "tz-offset":"-8",
+//         "dst":"A",
+//         "tz":"America/Los_Angeles"},
+//         "geometry":{
+//             "type":"Point",
+//             "coordinates":[-122.375,37.61899948120117]}}
+// ]};
+
+
+// // // Grabbing our GeoJSON data.
+// // L.geoJSON(sanFranAirport, {
+// //     // We turn each feature into a marker on the map.
+// //     pointToLayer: function(feature, latlng) {
+// //         console.log(feature);
+// //         return L.marker(latlng)
+// //         .bindPopup("<h2>" + feature.properties.city + ", " + feature.properties.country + "</h2>");
+// //     }
+// // }).addTo(map);
+
+// L.geoJson(sanFranAirport, {
+//     onEachFeature: function(feature, layer) {
+//       console.log(layer);
+//         layer.bindPopup("<h2>" + feature.properties.name + "</h2>");
+//      }
+// }).addTo(map);
+
+// // Coordinates for each point to be used in the line.
+// let line = [
+//     [33.9416, -118.4085],
+//     [37.6213, -122.3790],
+//     [40.7899, -111.9791],
+//     [47.4502, -122.3088]
+//   ];
+
+// // Create a polyline using the line coordinates and make the line red.
+// L. polyline(line, {
+//     color: "blue",
+//     opacity: .5,
+//     weight: 4,
+//     dashArray: 1,
+// }).addTo(map);
 
 //  Add a marker to the map for Los Angeles, California.
 // let marker = L.marker([34.0522, -118.2437]).addTo(map);
@@ -38,7 +78,7 @@ L. polyline(line, {
 // }).addTo(map);
 
 // Get data from cities.js
-let cityData = cities;
+// let cityData = cities;
 
 //   // Loop through the cities array and create one marker for each city.
 // cityData.forEach(function(city) {
@@ -64,6 +104,59 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-str
     maxZoom: 18,
     accessToken: API_KEY
 });
-// Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
 
+// We create the dark view tile layer that will be an option for our map.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+    Street: streets,
+    Dark: dark
+  };
+
+  // Create the map object with a center and zoom level. This is an alternative method to the above setView struture
+let map = L.map("mapid", {
+    center: [30, 30],
+    zoom: 2,
+    layers: [streets]
+  });
+
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+
+// // Then we add our 'graymap' tile layer to the map.
+// streets.addTo(map);
+
+// Accessing the airport GeoJSON URL
+let airportData = "https://raw.githubusercontent.com/Austin-Cyr/Mapping_Earthquakes/main/majorAirports.json";
+
+// Grabbing our GeoJSON data.
+d3.json(airportData).then(function(data) {
+    console.log(data);
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJson(data).addTo(map);
+});
+
+// // Grabbing our GeoJSON data.
+// d3.json(airportData).then(function(data) {
+//     console.log(data);
+//   // Creating a GeoJSON layer with the retrieved data.
+//   L.geoJson(data).addTo(map);
+// });
+
+// // Loop through the cities array and create one marker for each city.
+// // airportData.forEach(function(data) {
+// //     console.log(data)
+// //     L.marker(data.properties.city)
+// //     .bindPopup("<h2>Airport Code: " + feature.properties.faa + "</h2> <hr> <h3>Airport Name:" + feature.properties.name + "</h3>")
+// //     .addTo(map);
+// //    });
+
+// function onEachFeature(features, layer) {
+//     layer.bindPopup("<h2>Airport Code: " + features.properties.faa + "</h2> <hr> <h3>Airport Name:" + features.properties.name + "</h3>")
+//     .addTo(map);
+//    };
